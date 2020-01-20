abde0f7deff8c7080cf77931ab8ed46f866a47f5
stiehlrobot <olli.ohls@gmail.com>
2020-01-20 22:13:18 +0200

    migrated database to postgres drom default sqlite3

--- producthunter/settings.py
+++ producthunter/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'producthunter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'producthuntdb',
+        'USER': 'postgres',
+        'PASSWORD': 'Brandy887!',
+        'HOST': 'localhost',
+        'PORT':'5432',
     }
 }
 
