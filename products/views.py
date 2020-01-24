d36b2d5d7cd72a893a1c15cb5e9fc8ad726ffe29
stiehlrobot <olli.ohls@gmail.com>
2020-01-25 00:17:57 +0200

    added a button with functionality to transfer authed user to create product page. Also modified views.py with detail function to fetch and return product object with specific id.

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 
 from .models import Product
 
@@ -9,17 +10,28 @@ def home(request):
 
 @login_required
 def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
+            product = Product()
+            product.title = request.POST['title']
+            product.body = request.POST['body']
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://' + request.POST['url']
+            product.icon = request.FILES['icon']
+            product.icon = request.FILES['image']
+            product.pub_date = timezone.datetime.now()            
+            product.hunter = request.user
+            product.save()
+            return redirect('/products/' + str(product.id))
+
+        else:
+            return render(request, 'products/create.html', {'error':'All fields must be filled'})
     return render(request, 'products/create.html')
     
-'''
-def present(request):
 
-    products = Product.objects
-    return render(request, 'product/presentproducts.html', {'products':products})
-
-def productdetail(request):
+def detail(request, product_id):
 
     product_detail = get_object_or_404(Product, pk=product_id)
-    return render(request, 'product/productdetail.html', {'product':product_detail})
-
-'''
+    return render(request, 'products/detail.html', {'product':product_detail})
